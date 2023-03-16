da4de4a6afd8161e7784fbadcb6c9f76a02489b6
riimoon <mishay50@gmail.com>
2023-03-16 23:03:10 +0000

    my script is working

--- day1/newfile.sh
+++ day1/newfile.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 dir=$(find / -type d -name $1)
 cd $dir
-#
  
-git pull #get the latest
-git add $(pwd)  #adding files we want to push
-git commit -m $2 ##add a comment and use #1 as a message by user input
-git push -u origin main #pushing onto github
+git pull 
+git add . 
+git commit -m "$2" 
+git push -u origin main 
